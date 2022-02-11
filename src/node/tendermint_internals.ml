a70a869249e2df04399d76b652a29b04820a0378
Maria Climent-Pommeret <maria@marigold.dev>
2022-02-11 10:20:20 +0100

    [Tendermint] basic value handling

---
+++ src/node/tendermint_internals.ml
@@ -0,0 +1,18 @@
+open Protocol
+
+(** Tendermint sometimes decides on a `Nil` value. *)
+type value =
+  | Block of Protocol.Block.t
+  | Nil
+[@@deriving yojson]
+
+let string_of_value = function
+  | Nil -> "nil"
+  | Block b ->
+    Printf.sprintf "block %s" (Crypto.BLAKE2B.to_string b.Protocol.Block.hash)
+
+(* TODO: FIXME: Tendermint *)
+let repr_of_value v = v
+
+(* FIXME: Tendermint (this is copied bad design)*)
+let produce_value : (State.t -> value) ref = ref (fun _ -> assert false)
